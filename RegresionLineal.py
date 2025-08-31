a266ca9f579265ea44efc6b1b8038ad2b207b27e
OlimacJuan <jc.cortesv1@uniandes.edu.co>
2025-08-31 14:48:45 -0500

    Crear funcion: prueba_significancia_individual

--- RegresionLineal.py
+++ RegresionLineal.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import t 
 
 
 def calcular_betas(X: pd.DataFrame, y: pd.Series, incluir_intercepto=True) -> np.ndarray:
@@ -84,7 +85,46 @@ def matriz_covarianza_betas(X: pd.DataFrame, varianza: float) -> np.ndarray:
     """
     X_matrix = X.to_numpy()
     
-    if varianza is None:
-        varianza = calcular_varianza(X, y, betas)
     matriz_covarianza = varianza * np.linalg.inv(X_matrix.T @ X_matrix)
     return matriz_covarianza
+
+
+def prueba_significancia_individual(betas: np.ndarray, matriz_covarianza: np.ndarray, n: int, p=None, nivel_significancia=0.05) -> np.ndarray:
+    """
+    Realiza la prueba de significancia individual para cada coeficiente beta.
+
+    :param betas: Coeficientes estimados del modelo (p x 1).
+    :type betas: np.ndarray
+    :param matriz_covarianza: Matriz de covarianza de los coeficientes (p x p).
+    :type matriz_covarianza: np.ndarray
+    :param n: Número de observaciones.
+    :type n: int
+    :param nivel_significancia: Nivel de significancia para la prueba.
+    :type nivel_significancia: float
+
+    :return: Estadísticos t y p-valores para cada coeficiente.
+    :rtype: np.ndarray
+    """
+    # Crear un DataFrame para almacenar los resultados de la prueba
+    resultados = pd.DataFrame(columns=["Estadístico de prueba", "Valor critico", "Rechazo H0"])
+    
+    # Si no se proporciona el número de parámetros (p), se calcula como la longitud de los coeficientes beta
+    if p is None:
+        p = len(betas)
+    
+    # Calcular el valor crítico t basado en el nivel de significancia y los grados de libertad
+    valor_critico = t.ppf(1 - nivel_significancia / 2, df=n - p)
+    
+    # Iterar sobre cada coeficiente beta para realizar la prueba de significancia
+    for j in range(len(betas)):
+        # Calcular el estadístico de prueba t para el coeficiente beta j
+        estadistico_prueba = betas[j] / np.sqrt(matriz_covarianza[j, j])
+        
+        # Determinar si se rechaza la hipótesis nula (H0) para el coeficiente beta j
+        rechazo_h0 = abs(estadistico_prueba) > valor_critico
+
+        # Almacenar los resultados en el DataFrame
+        resultados.loc[j] = [estadistico_prueba, valor_critico, rechazo_h0]
+
+    # Retornar el DataFrame con los resultados de la prueba
+    return resultados
